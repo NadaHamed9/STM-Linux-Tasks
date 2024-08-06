022d97029cd9b62cc33d0d29d3eab4f0f480e174
Nada Hamed <Nada.Al-Baih01@eng-st.cu.edu.eg>
2024-08-07 02:15:31 +0300

    Add files via upload

---
+++ task2/function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int var1=5;
+int var2=3;
+
+int sum()
+{
+return var1+var2;
+}
---
+++ task2/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int sum();
+
+int main()
+{
+int x=0;
+x=sum();
+printf("x= %d\n",x);
+return 0;
+
+}
