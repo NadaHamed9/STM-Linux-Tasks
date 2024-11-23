b585ecf42af387e5615474bdf25d7f1056601543
Nada Hamed <Nada.Al-Baih01@eng-st.cu.edu.eg>
2024-11-23 20:13:36 +0200

    Add files via upload

---
+++ HTTPserver/greeting.cgi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Print the HTTP header first
+    printf("Content-Type: text/plain\r\n\r\n");
+    
+    // Print the greeting message
+    printf("Hello, World!\n");
+
+    return 0;
+}
+
